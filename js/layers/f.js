4cc238b5c796839749472a364c291d2bbdd36da2
RTXT25 <78957603+RTXT25@users.noreply.github.com>
2021-11-05 11:52:32 +1100

    Update f.js

--- js/layers/f.js
+++ js/layers/f.js
@@ -110,7 +110,7 @@ addLayer("f", {
         cols: 4,
         11: {
             title: "Discord",
-            cost() { return getBuyableAmount("f", 11).add(1) },
+            cost() { return getBuyableAmount("f", 11).add(0) },
             display() { return getBuyableAmount("f", 11).gte(1) ? `Each alt account raises your discord effect on fan gain to the ^1.1 power.<br/><br/>Next upgrade cost: ${this.cost()} fame` : `Create a discord, boosting your fan gain the more fans you have<br/><br/>Unlock cost: ${this.cost()} fame` },
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             effect() {
@@ -126,7 +126,7 @@ addLayer("f", {
         },
         12: {
             title: "Patreon",
-            cost() { return getBuyableAmount("f", 12).add(1).mul(2) },
+            cost() { return getBuyableAmount("f", 12).add(0).mul(1) },
             display() { return getBuyableAmount("f", 12).gte(1) ? `Each alt account raises your patreon effect on cash gain to the ^1.1 power.<br/><br/>Next upgrade cost: ${this.cost()} fame` : `Create a patreon, boosting your cash gain the more fans you have<br/><br/>Unlock cost: ${this.cost()} fame` },
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             effect() {
@@ -140,7 +140,7 @@ addLayer("f", {
         },
         13: {
             title: "Twitch",
-            cost() { return getBuyableAmount("f", 13).add(1).mul(2) },
+            cost() { return getBuyableAmount("f", 13).add(0).mul(1) },
             display() { return getBuyableAmount("f", 13).gte(1) ? `Each alt account raises your twitch effect on experience gain to the ^1.1 power.<br/><br/>Next upgrade cost: ${this.cost()} fame` : `Create a twitch where you stream development and get instant feedback, boosting your experience gain the more fans you have<br/><br/>Unlock cost: ${this.cost()} fame` },
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             effect() {
@@ -154,7 +154,7 @@ addLayer("f", {
         },
         14: {
             title: "Github",
-            cost() { return getBuyableAmount("f", 14).add(1).mul(3) },
+            cost() { return getBuyableAmount("f", 14).add(0).mul(1) },
             display() { return getBuyableAmount("f", 14).gte(1) ? `Each alt account raises your github effect on update gain to the ^1.1 power.<br/><br/>Next upgrade cost: ${this.cost()} fame` : `Add a link in the game to the github repo, boosting your update gain the more fans you have<br/><br/>Unlock cost: ${this.cost()} fame` },
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             effect() {
@@ -176,7 +176,7 @@ addLayer("f", {
             currencyDisplayName: "updates",
             currencyInternalName: "points",
             currencyLocation() { return player.u },
-            cost: new Decimal(15000),
+            cost: new Decimal(1),
             unlocked() { return hasUpgrade("g", 12) && !inChallenge("d", 11) },
             effect() { return player.f.fans.clampMin(10).log10().log(1.5).mul(layers.g.effect()).add(1) }
         },
@@ -186,7 +186,7 @@ addLayer("f", {
             currencyDisplayName: "updates",
             currencyInternalName: "points",
             currencyLocation() { return player.u },
-            cost: new Decimal(20000),
+            cost: new Decimal(2),
             unlocked() { return hasUpgrade("g", 12) && !inChallenge("d", 11) },
             effect() { return player.f.fans.clampMin(10).log10().log2().mul(layers.g.effect()).add(1) }
         },
@@ -196,7 +196,7 @@ addLayer("f", {
             currencyDisplayName: "updates",
             currencyInternalName: "points",
             currencyLocation() { return player.u },
-            cost: new Decimal(30000),
+            cost: new Decimal(3),
             unlocked() { return hasUpgrade("g", 12) && !inChallenge("d", 11) },
             effect() { return player.f.fans.clampMin(10).log10().log2().sqrt().mul(layers.g.effect()).add(1) }
         },
@@ -206,7 +206,7 @@ addLayer("f", {
             currencyDisplayName: "updates",
             currencyInternalName: "points",
             currencyLocation() { return player.u },
-            cost: new Decimal(50000),
+            cost: new Decimal(5),
             unlocked() { return hasUpgrade("g", 12) && !inChallenge("d", 11) },
             effect() { return player.f.fans.clampMin(10).log10().log10().mul(layers.g.effect()).add(1) }
         }
